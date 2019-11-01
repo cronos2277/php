3464e98566aa11d4f51be3b9648f8e2e1d7a3137
Adam Skynew <cronos2277@gmail.com>
2019-10-31 23:48:49 -0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -158,6 +158,25 @@ $URL_ATUAL= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	 * quando o envio for GET os parametros ja vai na URL, e POST os parametros vai dentro
 	 * da função send. Com ajax você consegue atualizar páginas sem precisar recarregar, 
 	 * consegue pegar recursos sem sair da página, assim como enviar um formulário sem sair da página.
-	 * */
+	 Sobre os métodos de abertura do fopen: 
+	 
+	 'r' 	Abre somente para leitura; coloca o ponteiro do arquivo no começo do arquivo.
+	'r+' 	Abre para leitura e escrita; coloca o ponteiro do arquivo no começo do arquivo.
+	'w' 	Abre somente para escrita; coloca o ponteiro do arquivo no começo do arquivo e reduz o comprimento do arquivo para zero. 
+		Se o arquivo não existir, tenta criá-lo.
+		
+	'w+' 	Abre para leitura e escrita; coloca o ponteiro do arquivo no começo do arquivo e reduz o comprimento do arquivo para zero. 
+		Se o arquivo não existir, tenta criá-lo.
+		
+	'a' 	Abre somente para escrita; coloca o ponteiro do arquivo no final do arquivo. Se o arquivo não existir, tenta criá-lo.
+	'a+' 	Abre para leitura e escrita; coloca o ponteiro do arquivo no final do arquivo. Se o arquivo não existir, tenta criá-lo.
+	'x' 	Cria e abre o arquivo somente para escrita; coloca o ponteiro no começo do arquivo. Se o arquivo já existir, a chamada a fopen() falhará, 
+		retornando FALSE e gerando um erro de nível E_WARNING. Se o arquivo não existir, tenta criá-lo.		
+		Isto é equivalente a especificar as flags O_EXCL|O_CREAT para a chamada de sistema open(2).
+	'x+' 	Cria e abre o arquivo para leitura e escrita; coloca o ponteiro no começo do arquivo. Se o arquivo já existir, a chamada a fopen() falhará, 
+		retornando FALSE e gerando um erro de nível E_WARNING. Se o arquivo não existir, tenta criá-lo. Isto é equivalente a especificar as flags 
+		O_EXCL|O_CREAT para a chamada de sistema open(2)
+	 
+	 */
 
 } ?>
