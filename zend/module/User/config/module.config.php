0fc92172e4069a4726504c176025a378b6d7defe
JotaPe <cronos2277@gmail.com>
2020-01-13 10:39:25 -0300

    zend updates

--- zend/module/User/config/module.config.php
+++ zend/module/User/config/module.config.php
@@ -1,1 +1,33 @@
-<?php return[];
+<?php 
+namespace User;
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
+return[
+    'router' => [
+        'routes' =>[
+            'user' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route' => '/user',
+                    'default' => [
+                        'controller' => Controller\IndexController::class,
+                        'action' => 'register'
+                    ]
+                    ],
+                    'may_terminate' => true,
+                    'child_routes' => [
+                        'default' => [
+                            'type' => Segment::class,
+                            'options' => [                                
+                                'route' => '[/:action][/token/:token]',
+                                'constaints' => [
+                                    'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                    'token' => '[[a-f0-9]{32}]$'
+                                ]
+                            ]
+                        ]
+                    ]
+            ]
+        ]
+    ]
+];
