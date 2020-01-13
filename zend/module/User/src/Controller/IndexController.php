6a0b2abb1e6dd9ff9d6176d482b6ab97f9c002d9
JotaPe <cronos2277@gmail.com>
2020-01-13 12:07:40 -0300

    user

--- zend/module/User/src/Controller/IndexController.php
+++ zend/module/User/src/Controller/IndexController.php
@@ -6,8 +6,34 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
-    public function indexAction()
+    public function registerAction()
+    {
+        return new ViewModel();
+    }
+    public function recoveredPasswordAction()
+    {
+        return new ViewModel();
+    }
+    public function newPasswordAction()
+    {
+        return new ViewModel();
+    }
+    public function confirmedEmailAction()
     {
         return new ViewModel();
     }
 }
+/*
+    Todas as acoes deve terminar com action, 
+    do contrario o Zend ira interpretar como
+    um metodo comum. Toda a acao eh referente
+    a uma view, ou seja cada acao aqui vai ter
+    a sua propria view. Outra coisa, o zend vai 
+    procurar pelos seguintes arquivos: 
+    'confirmed-email.phtml','new-password.phtml',
+    'recovered-password','register.phtml', repare
+    que o zend ele coloca um hifen no nome dos 
+    arquivos quando tem camel case, ou seja 
+    uma action aqui chamado de minhaZendAction,
+    se transforma no arquivo 'minha-zend.phtml'.
+*/
