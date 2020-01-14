66b5f43381dac41cd684e96a23aca03eb496afbe
JotaPe <cronos2277@gmail.com>
2020-01-14 13:38:06 -0300

    zend criando modulo explicacao concluido

--- #/projeto-zend/config/modules.config.php
+++ #/projeto-zend/config/modules.config.php
@@ -11,7 +11,19 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
-    'Zend\Router',
-    'Zend\Validator',
-    'Application',
+    'Zend\Router', //Modulos da Zend.
+    'Zend\Validator', //Modulos da Zend.
+    'Application', //Esse ja vem por padrao. Serve como exemplo no eskeleton.
+    'Modulo', // Esse eh o metodo criado aqui, e aqui eh feito o registro do mesmo
 ];
+/*
+    Todos os modulos locais, devem ser registrado aqui. 
+    Existe dois modules.config.php, o local configurado em cada modulo
+    e este aqui que eh o global, ou seja os modulos devem estar registrados
+    aqui para que funcionem, mas uma vez configurado aqui, nao esqueca
+    de tambem fazer as configuracoes do composer.json, na parte do autoload
+    no PSR4, como eh esse exemplo, apos configurado, nao esqueca do comando
+    `composer dump-autoload` para atualizar os loaders do composer, toda vez
+     que voce altera isso, se faz necessario executar esse comando, para que o
+     composer atualize dentro dos arquivos PHP os novos arquivos a serem carregados.
+*/
---
+++ #/projeto-zend/module/Modulo/view/modulo/modulo/readme.md
@@ -0,0 +1,15 @@
+<h1>Sobre a Pasta de Views</h2>
+<p>Todas as subpastas aqui devem ter nomes minusculos e a hierarquia
+de diretorios eh a seguinte:</p> 
+<p>a primeira pasta modulo, uma pasta com
+o nome do seu modulo, nesse caso como esse modulo se chama Modulo,
+esse primeiro nome eh modulo(minusculo).</p>
+<p>
+a respeito da segunda pasta, a pasta modulo, dentro de uma outra pasta modulo, sendo essa uma sub-pasta de mesmo nome, isso acontece porque
+o controller se chama modulo tambem, mas na view dentro da pasta do modulo aqui dentro, para cada controller criado, se faz necessario criar
+uma pasta para cada controller, repare que cada controller criado, tera
+a sua respectiva pasta dentro da view, estando todas essas pastas dentro
+da pasta do modulo, isso tudo dentro dessa pasta view. Assim sendo essa 
+eh a logica por de tras da hierarquia da pasta view.
+[view]/[pasta_do_modulo_minusculo]/[pasta_de_cada_controlador]/[arquivos.phtml]
+</p>
