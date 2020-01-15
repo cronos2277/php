4aae40418096ac83a4b18b8ca6af792cc89097ad
JotaPe <cronos2277@gmail.com>
2020-01-15 09:25:11 -0300

    Rotas 2

--- #/projeto-zend/module/Modulo/src/Controller/ModuloController.php
+++ #/projeto-zend/module/Modulo/src/Controller/ModuloController.php
@@ -1,11 +1,18 @@
 <?php
 namespace Modulo\Controller;
-
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 //Recomenda-se extender AbstractActionController ou alguma classe
 //como essa ou implementar uma interface com essa funcionalidade, 
 //para que seja feito coisas como tratamento de errose etc... 
+/*
+    O nome desse arquivo se chama ModuloController.php, logo
+    o nome que vem antes do sufixo Controller, eh o que determina
+    a nomeacao da pasta, se fosse por exemplo AplicacaoController,
+    a pasta das view estaria em modulo/src/view/modulo/aplicacao, 
+    lembrando que esse view esta no mesmo nivel hierarquico de diretorio
+    que a pasta src, ambos dentro da pasta do modulo. 
+*/
 class ModuloController extends AbstractActionController{
     /* 
         Todo o metodo controller deve retornar uma view.
@@ -13,7 +20,7 @@ class ModuloController extends AbstractActionController{
         mencionado como index.
     */
     public function indexAction()
-    {
+    { //rota http://URL:porta/modulo
         //regras de negocio...
         return new ViewModel();
         /*
@@ -26,6 +33,8 @@ class ModuloController extends AbstractActionController{
     }
     public function addAction()
     {
+        //rota http://URL:porta/modulo/add
+        // Arquivo de view: modulo/src/view/modulo/modulo/add.phtml
         /*
             Aqui esta o add action, que pode ser acessado
             na seguinte URL, localhost:porta/modulo/add,
@@ -37,8 +46,32 @@ class ModuloController extends AbstractActionController{
             nao eh passado parametro, sera procurado um arquivo
             chamado add.phtml dentro de 
             /view/[pasta-modulo-nome]/[pasta-controller-nome]
-            seguindo essa ordem claro.
+            seguindo essa ordem claro. Lembrando que como 
+            o nome desse arquivo eh ModuloController.php, logo
+            a pasta que o mesmo espera receber na pasta de
+            controller, se chama Modulo.
+
         */
+        return new ViewModel();
+    }
+    public function saveAction(){
+    //rota http://URL:porta/modulo/save   
+    // Arquivo de view: modulo/src/view/modulo/modulo/save.phtml
+        return new ViewModel();
+    }
+    public function editAction(){
+    //rota http://URL:porta/modulo/edit    
+    // Arquivo de view: modulo/src/view/modulo/modulo/edit.phtml
+        return new ViewModel();
+    }
+    public function removeAction(){
+    //rota http://URL:porta/modulo/remove    
+    // Arquivo de view: modulo/src/view/modulo/modulo/remove.html
+        return new ViewModel();
+    }
+    public function confirmAction(){
+    //rota http://URL:porta/modulo/confirm    
+    // Arquivo de view: modulo/src/view/modulo/modulo/confirm.phtml
         return new ViewModel();
     }
 }
