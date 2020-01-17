6c52ecc760e09e05ab0c9bdd3be6dbb2b7293f93
JP Windows <cronos2277@gmail.com>
2020-01-17 12:11:56 -0300

    senha do password a tona de volta

--- #/projeto-zend/module/Modulo/config/module.config.php
+++ #/projeto-zend/module/Modulo/config/module.config.php
@@ -110,7 +110,7 @@ return [
         'driver' => 'Pdo_Mysql', //Driver PHP do banco de dados
         'database' => 'zend', //DB
         'username' => 'root',
-        'password' => '',
+        'password' => '123456',
         'hostname' => 'localhost' //Host.
         //Tambem tem o campo port e charset se precisar
         //para mais informações: https://docs.zendframework.com/zend-db/adapter/
