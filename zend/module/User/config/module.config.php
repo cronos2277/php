08080fc902d7979a74b4a57d066b3adfd2929cb2
JotaPe <cronos2277@gmail.com>
2020-01-27 10:04:32 -0300

    Rotas

--- zend/module/User/config/module.config.php
+++ zend/module/User/config/module.config.php
@@ -1,2 +1,34 @@
 <?php
-return [];
+namespace User;
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
+
+return [
+    'router' => [
+        'routes' => [
+            'user' => [
+                'type' => Literal::class,
+                'options' =>[
+                    'route' => '/user',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action' => 'register'
+                    ]
+                ],
+                'may_terminate' => true,
+                'child_routes' =>[
+                    'default' => [
+                        'type' => Segment::class,
+                        'options' =>[
+                            'route' => '[/:action][/token/:token]',
+                            'constraints' => [
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'token' => '[[a-f0-9]{32}]$'
+                            ]
+                        ]
+                    ]
+                ]
+            ],            
+        ]
+    ]
+];
