2cc0bb04ee361848a9f76d936bf7e12836ba0dbe
Jesus Vieira <jesusvieiradelima@gmail.com>
2020-01-31 08:56:54 -0300

    Corrigindo erro de rota USER

--- zend/module/User/config/module.config.php
+++ zend/module/User/config/module.config.php
@@ -1,8 +1,10 @@
 <?php
 namespace User;
+
 use Zend\Router\Http\Literal;
 use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
+
 return [
     'router' => [
         'routes' => [
@@ -23,28 +25,28 @@ return [
                             'route' => '[/:action][/token/:token]',
                             'constraints' => [
                                 'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'token' => '[[a-f0-9]{32}]$'
+                                'token' => '[a-f0-9]{32}$'
                             ]
                         ]
                     ]
                 ]
-            ],            
-        ],
-        'controllers' => [
-            'factories' => [
-                Controller\IndexController::class => InvokableFactory::class
-            ]
-        ],
-        'view_manager' => [
-            'template_map' => [
-                'user/index/confirmated-email' => __DIR__.'/../view/user/index/confirmated-email.phtml',
-                'user/index/new-password' => __DIR__.'/../view/user/index/new-password.phtml',
-                'user/index/recovered-password' => __DIR__.'/../view/user/index/recovered-password.phtml',
-                'user/index/register' => __DIR__.'/../view/user/index/register.phtml'
             ],
-            'template_path_stack' => [
-                __DIR__.'/../view',
-            ]
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class
+        ]
+    ],
+    'view_manager' => [
+        'template_map' => [
+            'user/index/confirmated-email' => __DIR__.'/../view/user/index/confirmated-email.phtml',
+            'user/index/new-password' => __DIR__.'/../view/user/index/new-password.phtml',
+            'user/index/recovered-password' => __DIR__.'/../view/user/index/recovered-password.phtml',
+            'user/index/register' => __DIR__.'/../view/user/index/register.phtml'
+        ],
+        'template_path_stack' => [
+            __DIR__.'/../view',
         ]
     ]
 ];
--- zend/module/User/src/Controller/IndexController.php
+++ zend/module/User/src/Controller/IndexController.php
@@ -22,5 +22,4 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
-
 }
