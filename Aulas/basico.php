98f290ae49d1792a07b385bfff8c0622fd063fa5
JP Windows <cronos2277@gmail.com>
2020-03-17 18:13:15 -0300

    novo arquivo basico

---
+++ Aulas/basico.php
@@ -0,0 +1,8 @@
+<?php 
+//PHP Com valores Inteiros.
+echo PHP_INT_MAX; #Valor maximo para um inteiro.
+echo PHP_INT_MIN; #Valor minimo para um inteiro.
+echo 77; # Sem zero a esquerda, o numero eh decimal, valor "77".
+echo 077; # Com zero a esquerda, o numero eh em base Octal, valor "63".
+echo 0b1111; #Com 0b o numero esta em base binaria, valor "15".
+echo 0x1ABcafe9; #com 0x o PHP interpreta como hexadecimal, o valor eh: "448573417"
