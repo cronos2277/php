ba5f8850fe784b8dcc795147665f7343adaba794
JP Windows <cronos2277@gmail.com>
2020-09-17 14:48:50 -0300

    Obtendo usuários do Banco de dados

--- Projetos/Innout - PHP/public/index.php
+++ Projetos/Innout - PHP/public/index.php
@@ -1,12 +1,4 @@
 <?php
 require_once(dirname(__FILE__,2).'/src/config/config.php');
 require_once(dirname(__FILE__,2).'/src/models/User.php');
-$sql = "select * from users";
-$result = Database::getResultFromQuery($sql);
-while($row = $result->fetch_assoc()){
-    print_r($row);
-    echo "<br>";
-}
-
-$user = new User(['name' => 'nome', 'email' => 'email@email']);
-print_r($user);
+print_r(User::get());
--- Projetos/Innout - PHP/src/models/Model.php
+++ Projetos/Innout - PHP/src/models/Model.php
@@ -26,18 +26,35 @@ class Model{
         $this->values[$key] = $value;
     }
 
-    public static function getSelect($filters = [], $columns = '*'){
+    public static function get($filters = [],$columns = "*"){
+        $objects = [];
+        $result = static::getResultSetFromSelect($filters,$columns);
+        if($result){
+            $class = get_called_class();
+            while($row = $result->fetch_assoc()){
+                array_push($objects,new $class($row));
+            }
+        }
+        return $objects;
+    }
+
+    public static function getResultSetFromSelect($filters = [], $columns = '*'){
         $sql = "SELECT ${columns} FROM "
         .static::$tableName
         .static::getFilters($filters);
-        return $sql;
+        $result = Database::getResultFromQuery($sql);
+        if($result->num_rows === 0){
+            return null;
+        }else{
+            return $result;
+        }
     }
 
     private static function getFilters($filters){
         $sql = ' WHERE 1 = 1'; //Aqui eh adicionado o where a query, nao apague.
         if(count($filters) > 0){
             foreach($filters as $column=>$value){
-                $sql .= "AND ${column} =".static::getFormattedValue($value);
+                $sql .= " AND ${column} =".static::getFormattedValue($value);
             }
         }
         return $sql;
