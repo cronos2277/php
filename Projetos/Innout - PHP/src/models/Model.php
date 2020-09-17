da2e5b45ba9b0272e08a7755b49f079102f132a5
JP Windows <cronos2277@gmail.com>
2020-09-17 13:48:08 -0300

    Gerando comando SQL

--- Projetos/Innout - PHP/src/models/Model.php
+++ Projetos/Innout - PHP/src/models/Model.php
@@ -25,4 +25,31 @@ class Model{
     public function __set($key,$value){
         $this->values[$key] = $value;
     }
+
+    public static function getSelect($filters = [], $columns = '*'){
+        $sql = "SELECT ${columns} FROM "
+        .static::$tableName
+        .static::getFilters($filters);
+        return $sql;
+    }
+
+    private static function getFilters($filters){
+        $sql = ' WHERE 1 = 1'; //Aqui eh adicionado o where a query, nao apague.
+        if(count($filters) > 0){
+            foreach($filters as $column=>$value){
+                $sql .= "AND ${column} =".static::getFormattedValue($value);
+            }
+        }
+        return $sql;
+    }
+
+    private static function getFormattedValue($value){
+        if(is_null($value)){
+            return "null";
+        }else if(gettype($value) === 'string'){
+            return "'${value}'";
+        }else{
+            return $value;
+        }
+    }
 }
