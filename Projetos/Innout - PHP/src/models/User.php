e9842e464b21078b3d0726dcd43e17692aeef669
JP Windows <cronos2277@gmail.com>
2020-09-17 12:54:00 -0300

    Classe User

---
+++ Projetos/Innout - PHP/src/models/User.php
@@ -0,0 +1,12 @@
+<?php
+echo MODEL_PATH;
+require_once(realpath(MODEL_PATH.'/Model.php'));
+class User extends Model{
+    protected static $tableName = 'users';
+    protected static $columns = [
+        'id','name','password','email',
+        'start_date','end_date','is_admin'
+    ];
+
+
+}
