db64c1470618e81ea8e44bc8ac9ae856bed37e63
JP Windows <cronos2277@gmail.com>
2020-10-05 21:14:51 -0300

    Cabeçalho #1

---
+++ Projetos/Innout - PHP/src/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>CRONOS2277 (João Paulo)</span>
+</footer>
+</body>
+</html>
---
+++ Projetos/Innout - PHP/src/views/template/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>In N' Out</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="assets/css/comum.css" rel="stylesheet"/>
+    <link href="assets/css/icofont.min.css" rel="stylesheet"/>
+    <link href="assets/css/template.css" rel="stylesheet"/>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <i class="icofont-travelling mr-2"></i>
+            <span class="font-weight-light">In</span>
+            <span class="font-weight-bold mr-2 ml-2">N'</span>
+            <span class="font-weight-light">Out</span>
+            <i class="icofont-runner-alt-1 ml-2"></i>
+        </div>
+        <div class="menu-toggle mx-3">
+            <i class="icofont-navigation-menu"></i>
+        </div>
+        <div class="spacer"></div>
+    </header>
