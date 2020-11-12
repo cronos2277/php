352794a9a4f84a501fc727e190fd21aae63cff5e
JP Windows <cronos2277@gmail.com>
2020-11-11 23:31:14 -0300

    Sessao do usuario

--- Projetos/Innout - PHP/public/index.php
+++ Projetos/Innout - PHP/public/index.php
@@ -2,8 +2,8 @@
 require_once(dirname(__FILE__,2).'/src/config/config.php');
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-if($uri === '/' || $uri === '' || $uri === 'index.php'){
-    $uri = '/login.php';
+if($uri === '/' || $uri === '' || $uri === 'index.php' || $uri === '/index.php'){
+    $uri = 'day_records.php';
 }
 
 require_once(dirname(__FILE__,2)."/src/controllers/{$uri}");
