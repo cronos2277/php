71945de064dbcf5a9ac724d4cd7281aab313b5ad
JP Windows <cronos2277@gmail.com>
2020-11-14 16:36:25 -0300

    Relatorio Mensal #1

--- Projetos/Innout - PHP/public/assets/js/app.js
+++ Projetos/Innout - PHP/public/assets/js/app.js
@@ -24,7 +24,7 @@
             const parts = active.innerHTML.split(':');
             active.innerHTML = addOneSecond(parts[0],parts[1],parts[2]);
         },1000);
-    }
+    }     
 })();
 
 
---
+++ Projetos/Innout - PHP/src/controllers/monthly_report.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+requireValidSession();
+loadTemplateView('monthly_report');
