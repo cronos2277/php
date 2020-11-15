63d5295a8cf486f4e388762cc7e8a384016f30b4
JP Windows <cronos2277@gmail.com>
2020-11-14 22:20:39 -0300

    Relatorio Mensal #3

--- Projetos/Innout - PHP/src/controllers/monthly_report.php
+++ Projetos/Innout - PHP/src/controllers/monthly_report.php
@@ -1,6 +1,37 @@
 <?php
 session_start();
 requireValidSession();
+$currentDate = new DateTime();
 $user = $_SESSION['user'];
 $registries = WorkingHours::getMonthlyReport($user->id, new DateTime());
-loadTemplateView('monthly_report',['registries' => $registries]);
+
+$report = [];
+$workDay = 0;
+$sumOfWorkedTime = 0;
+$lastday = getLastDayOfMonth(($currentDate)->format('d'));
+for($day = 1; getLastDayOfMonth($currentDate)->format('d'); $day++){
+    $date = $currentDate->format('Y-m').'-'.sprintf('%2d',$day);
+    $registry = $registries[$date];
+    if(isPastWorkday($date)) $workDay++;
+    if($registry){
+        $sumOfWorkedTime += $registry->worked_time;
+        array_push($report,$registry);
+    }else{
+        array_push($report,new WorkingHours(
+            [
+                'work_date' => $date,
+                'worked_time' => 0
+            ]
+        ));
+    }
+}
+
+$expectedTime = $workDay * DAILY_TIME;
+$balance = getTimeStringFromSeconds(abs($sumOfWorkedTime - $expectedTime));
+$sign = ($sumOfWorkedTime >= $expectedTime) ? "+" : "-";
+
+loadTemplateView('monthly_report',[
+    'report' => $report,
+    'sumOfWorkedTime' => $sumOfWorkedTime,
+    'balance' => "{$sign}{$balance}"
+]);
