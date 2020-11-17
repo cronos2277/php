5c56e8306a860d93f107ad3a2e35c2803a734107
JP Windows <cronos2277@gmail.com>
2020-11-17 01:01:51 -0300

    Consultas gerenciais #1

--- Projetos/Innout - PHP/src/views/template/left.php
+++ Projetos/Innout - PHP/src/views/template/left.php
@@ -3,7 +3,7 @@
         <ul class="nav-list">
             <li class="nav-item"><a href="day_records.php"><i class="icofont-ui-check mr-2"></i>Registrar Ponto</a></li>
             <li class="nav-item"><a href="monthly_report.php"><i class="icofont-ui-calendar mr-2"></i>Relatório Mensal</a></li>           
-            <li class="nav-item"><a href=""><i class="icofont-chart-histogram mr-2"></i>Relatório Gerencial</a></li>           
+            <li class="nav-item"><a href="manager_report.php"><i class="icofont-chart-histogram mr-2"></i>Relatório Gerencial</a></li>           
             <li class="nav-item"><a href=""><i class="icofont-users mr-2"></i>Usuários</a></li>           
         </ul>
     </nav>
