92177da8bb5aef7a8a602a57afbb6323c605052c
JP Windows <cronos2277@gmail.com>
2021-03-03 23:19:23 -0300

    Base feita

---
+++ Aulas/#LARAVEL/formulario/database/migrations/2021_03_04_012259_produtos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Produtos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {            
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->integer('estoque');
+            $table->integer('categoria_id')->unsigned()->nullable();
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
